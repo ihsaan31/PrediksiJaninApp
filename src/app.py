0a3b48bfb4ed44e6f6dc3e0a609b0146d34d0db3
ihsaan31 <ihsanzombieihsan@gmail.com>
2023-12-09 16:37:18 +0700

    Attemp to remove .env from tracking

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ import joblib
 import gspread
 from datetime import datetime
 from dotenv import load_dotenv, dotenv_values
+from google.oauth2 import service_account
 import os
 # Load the model
 
@@ -390,7 +391,22 @@ def update_layout(selected_options_17, selected_options_18, n, is_open, usia_ibu
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         config = dotenv_values("/etc/secrets/.env")
-        client = gspread.service_account(filename=config)
+        credentials_dict = {
+           "type": config["type"],
+           "project_id": config["project_id"],
+           "private_key_id": config["private_key_id"],
+           "private_key": config["private_key"],
+           "client_email": config["client_email"],
+           "client_id": config["client_id"],
+           "auth_uri": config["auth_uri"],
+           "token_uri": config["token_uri"],
+           "auth_provider_x509_cert_url": config["auth_provider_x509_cert_url"],
+           "client_x509_cert_url": config["client_x509_cert_url"],
+           "universe_domain": config["universe_domain"]
+}
+        creds = service_account.Credentials.from_service_account_info(credentials_dict)
+        client = gspread.service_account_from_dict(credentials_dict)
+        #client = gspread.service_account(filename=config)
         sheets = client.open_by_key('1UMOEJvUrcuCOWZPiMpWlpEcIMKIm3p8eSBq0HjaFsmI')
         x = sheets.get_worksheet(0)
         riwayat_penyakit_join = ', '.join(riwayat_penyakit)
